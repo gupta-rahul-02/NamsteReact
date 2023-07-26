5b420c6fac3d369783c9d683305ad7f3e6c2c530
rahul gupta <rahulguptaslg20@gmail.com>
2023-07-26 13:59:38 +0530

    Inception Episode 1

---
+++ Inception_L1/App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement("h1", {
+    id:"title"
+  }, "Namaste React");
+
+  const heading2 = React.createElement("h2", {
+    id:"title2"
+  },"Namaste JS")
+
+  const container = React.createElement("div",{
+    id:"container"
+  },[heading,heading2])
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  //Parsing react element inside the root
+  root.render([container, heading]);
